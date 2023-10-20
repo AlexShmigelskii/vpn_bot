a437a1d03658adf296e989ee180e418d2439ff53
AlexShmigelskii <s5003626@gmail.com>
2023-10-21 00:33:12 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
+import secret
 from handlers import questions, different_types
 
 
 # Запуск бота
 async def main():
-    bot = Bot(token="6643419772:AAGQFMuDkoEXZPM41B-Gh7plw71Z4aAZGm4")
+    bot = Bot(token=secret.TOKEN)
     dp = Dispatcher()
 
     dp.include_routers(questions.router, different_types.router)
